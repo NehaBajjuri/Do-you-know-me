0d4c4e7360cfa2d5e55b72db4fdac306cab8326f
Neha Bajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2022-11-05 13:48:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var readlineSync = require("readline-sync");
+
+score = 0
+function welcome() {
+  var userId = readlineSync.question("May I have your name? ")
+  console.log("Welcome " + userId + "!!")
+  console.log("Hey " + userId + " Let's see how well you know me ;>")
+}
+//array of objects
+
+var questions = [{
+  question: "What is my name? ",
+  answer: "Neha"
+}, {
+  question: "What is my favourite food?",
+  answer: "dosa"
+},
+{
+  question: "What is my Birthday Month? ",
+  answer: "april"
+},
+{
+  question: "Which flavour do I like in icecreams?",
+  answer: "butterscotch"
+},
+{
+  question: "Who is my favourite heroine? ",
+  answer: "Anushka"
+}
+
+];
+
+
+function quiz(question, answer) {
+  var userAnswer = readlineSync.question(question);
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log("Yay!You were right!!")
+    score++;
+  }
+  else {
+    console.log("I'm sorry! You are wrong!!")
+
+  }
+
+  console.log("Current score:  ", score)
+  console.log("..................")
+}
+function game() {
+  for (var i = 0; i < questions.length; i++) {
+    var currentQuestion = questions[i];
+    quiz(currentQuestion.question, currentQuestion.answer)
+  }
+}
+
+welcome();
+game();
+
+console.log("Yayy! You scored ", score);
